fe60a3f2fb22cd4a1a28342bde097bf4f4806987
bw <thebw.42@pm.me>
2024-08-19 19:05:18 +0200

    configts commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import paths from 'vite-tsconfig-paths'
+// import paths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), paths()],
+  plugins: [react()],
+  base: '/2S_piggie/',
 })
